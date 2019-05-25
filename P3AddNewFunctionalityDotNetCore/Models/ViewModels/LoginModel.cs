05065e13d7bb4e3f151103935d4ab9b2283391aa
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-05-25 16:16:49 -0400

    updated loginmodel password property to be type password for view to display hidden characters

--- P3AddNewFunctionalityDotNetCore/Models/ViewModels/LoginModel.cs
+++ P3AddNewFunctionalityDotNetCore/Models/ViewModels/LoginModel.cs
@@ -8,6 +8,7 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         public string Name { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public string ReturnUrl { get; set; } = "/";
